bd2f687218daff20ca58fa4e7ff2e1d9832dba21
Alexandre Garbé <alexandre.garbe@epitech.eu>
2024-02-11 18:59:31 +0100

    feat: Created loop.c where the format is printed waiting for user inputs

--- src/shell/loop.c
+++ src/shell/loop.c
@@ -15,13 +15,13 @@ int shell_loop(char **env)
     char **args;
     int status = 1;
 
-    do {
-        my_putstr("minishell1> ");
+    for (int i = 0; status; i++) {
+        mini_printf("Minishell > ");
         line = read_line();
-        args = split_line(line);
-        status = execute(args, env);
+        args = str_to_wordarray(line, ' ');
+        status = execute_check(args, env);
         free(line);
         free(args);
-    } while (status);
+    }
     return 0;
 }
