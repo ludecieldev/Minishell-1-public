3e60e8fe1efa89da7f4609db98747c9b1f623074
Alexandre Garbé <alexandre.garbe@epitech.eu>
2024-02-11 17:19:27 +0100

    feat: Created include.h to include all the necessary lib

---
+++ include/include.h
@@ -0,0 +1,16 @@
+/*
+** EPITECH PROJECT, 2024
+** B-PSU-200-LIL-2-1-minishell1-alexandre.garbe
+** File description:
+** include.h
+** Author:
+** ludeciel
+*/
+
+#ifndef B_PSU_200_LIL_2_1_MINISHELL1_ALEXANDRE_GARBE_INCLUDE_H
+    #define B_PSU_200_LIL_2_1_MINISHELL1_ALEXANDRE_GARBE_INCLUDE_H
+    #include <stdio.h>
+    #include <stdlib.h>
+    #include <unistd.h>
+    #include <string.h>
+#endif //B_PSU_200_LIL_2_1_MINISHELL1_ALEXANDRE_GARBE_INCLUDE_H
