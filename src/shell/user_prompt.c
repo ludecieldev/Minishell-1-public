f0bf24190d82fb79bf718028851d1ff24ef54ba9
Alexandre Garbé <alexandre.garbe@epitech.eu>
2024-02-17 19:07:41 +0100

    feat: Updated user_prompt.c

--- src/shell/user_prompt.c
+++ src/shell/user_prompt.c
@@ -11,25 +11,7 @@
 
 extern char **environ;
 
-char *get_username(void)
-{
-    char *user = my_getenv("USER");
-    char *new = malloc(sizeof(char) * (my_strlen(user) - 5));
-    int i = 5;
-    int j = 0;
-
-    for (; user[i] != '\0'; i++) {
-        new[j] = user[i];
-        j++;
-    }
-    new[j] = '\0';
-    return (new);
-}
-
 void user_prompt(void)
 {
-    char *user = get_username();
-
-    mini_printf("user" "@%s $ ", user);
-    free(user);
+    mini_printf("$> ");
 }
