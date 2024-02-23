db6620dcc5160bdfabccc4c95b0979b2b37e3940
Alexandre Garbé <alexandre.garbe@epitech.eu>
2024-02-23 02:11:21 +0100

    feat : added a function to handle "setenv"

---
+++ src/shell/custom_args/setenv/my_setenv.c
@@ -0,0 +1,36 @@
+/*
+** EPITECH PROJECT, 2024
+** minishell
+** File description:
+** setenv.c
+** Author:
+** ludeciel
+*/
+
+#include "../../../../include/minishell.h"
+
+void my_setenv(char **args, char ***env, int *status)
+{
+    (void)status;
+    int i = 0;
+    char *new_var = NULL;
+
+    if (args[1] == NULL) {
+        setenv_no_args(args, env, status);
+        return;
+    }
+    for (; (*env)[i] != NULL; i++) {
+        if (my_strncmp(args[1], (*env)[i], my_strlen(args[1])) == 0) {
+            new_var = my_strcat(args[1], "=");
+            new_var = my_strcat(new_var, args[2]);
+            (*env)[i] = new_var;
+            return;
+        }
+    }
+    *env = my_realloc_array(*env, args[1]);
+    (*env)[i] = my_strcat(args[1], "=");
+    (*env)[i] = my_strcat((*env)[i], args[2]);
+    (*env)[i + 1] = NULL;
+}
+
+
