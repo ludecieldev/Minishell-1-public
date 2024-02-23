415da85a088f55b701350fa9267196192232f99d
Alexandre Garbé <alexandre.garbe@epitech.eu>
2024-02-23 02:04:02 +0100

    feat : add a function to handle "cd <directory>"

---
+++ src/shell/custom_args/cd/cd_directory.c
@@ -0,0 +1,21 @@
+/*
+** EPITECH PROJECT, 2024
+** minishell
+** File description:
+** cd_directory.c
+** Author:
+** ludeciel
+*/
+
+#include "../../../../include/minishell.h"
+
+void cd_directory(char **args, char ***env, int *status)
+{
+    if (chdir(args[1]) == -1) {
+        puterror(args[1]);
+        puterror(": No such file or directory.\n");
+        *status = 84;
+        return;
+    }
+    set_pwd(env, args[1]);
+}
