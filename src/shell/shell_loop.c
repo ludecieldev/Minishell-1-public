09bdfa5e2e2f3daea963251b322beef31a039d8e
Alexandre Garbé <alexandre.garbe@epitech.eu>
2024-02-23 02:12:54 +0100

    feat : added a function to loop the sh

---
+++ src/shell/shell_loop.c
@@ -0,0 +1,25 @@
+/*
+** EPITECH PROJECT, 2024
+** minishell
+** File description:
+** shell_loop.c
+** Author:
+** ludeciel
+*/
+
+#include "../../include/minishell.h"
+
+int shell_loop(char ***env, int *status)
+{
+    char *line = NULL;
+    char **args = NULL;
+
+    for (; status != 0;) {
+        mini_printf("$> ");
+        if ((line = read_line()) == NULL)
+            exit(*status);
+        args = str_to_wordarray(line, ' ');
+        *status = executecheck(args, env, status);
+    }
+    return 0;
+}
