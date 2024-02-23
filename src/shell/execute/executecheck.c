232fd013fd12fe10673842c834b5f105ce47ce2b
Alexandre Garbé <alexandre.garbe@epitech.eu>
2024-02-23 02:06:16 +0100

    feat : added a function to check if input is a special arg or a regular function

---
+++ src/shell/execute/executecheck.c
@@ -0,0 +1,25 @@
+/*
+** EPITECH PROJECT, 2024
+** minishell
+** File description:
+** execute.c
+** Author:
+** ludeciel
+*/
+
+#include "../../../include/minishell.h"
+
+int executecheck(char **args, char ***env, int *status)
+{
+    for (int i = 0; i < 3; i++) {
+        if (my_strcmp(args[0], specialargs[i].command) == 0) {
+            specialargs[i].func(args, env, status);
+            return 1;
+        }
+    }
+    if (args[0] == NULL) {
+        return 1;
+    }
+    *status = execute_bin(args, env);
+    return *status;
+}
