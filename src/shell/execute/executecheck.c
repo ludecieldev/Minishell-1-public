264c3ac85828cf8895b1a8b6eb81b9ef4009944b
Alexandre Garbé <alexandre.garbe@epitech.eu>
2024-02-27 01:49:19 +0100

    feat : updated returned status in executecheck.c

--- src/shell/execute/executecheck.c
+++ src/shell/execute/executecheck.c
@@ -11,15 +11,13 @@
 
 int executecheck(char **args, char ***env, int *status)
 {
-    for (int i = 0; i < 5; i++) {
+    for (int i = 0; i < 5; i++)
         if (my_strcmp(args[0], specialargs[i].command) == 0) {
             specialargs[i].func(args, env, status);
-            return 0;
+            return *status;
         }
-    }
-    if (args[0] == NULL) {
+    if (args[0] == NULL)
         return 0;
-    }
     *status = execute_bin(args, env);
     return *status;
 }
