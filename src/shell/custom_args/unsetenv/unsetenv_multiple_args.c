eca0ad5cfa967cb653120d664769b86825076457
Alexandre Garbé <alexandre.garbe@epitech.eu>
2024-02-27 01:51:34 +0100

    feat : updated unsetenv_multiple_args.c
    BREAKING CHANGE : unsetenv many now have it owns function and is not calling basic unsetenv anymore

--- src/shell/custom_args/unsetenv/unsetenv_multiple_args.c
+++ src/shell/custom_args/unsetenv/unsetenv_multiple_args.c
@@ -9,9 +9,29 @@
 
 #include "../../../../include/minishell.h"
 
-void unsetenv_multiple_args(char **args, char ***env, int *status)
+int should_unset(char *env_var, char **args)
 {
     for (int i = 1; args[i] != NULL; i++) {
-        unsetenv_onearg(&args[i], env, status);
+        if (my_strncmp(env_var, args[i], my_strlen(args[i])) == 0) {
+            return 1;
+        }
     }
+    return 0;
+}
+
+void unsetenv_multiple_args(char **args, char ***env, int *status)
+{
+    int j = 0;
+    char **newenv = malloc(sizeof(char *) * (my_arraylen(*env) + 1));
+
+    for (int i = 0; (*env)[i] != NULL; i++) {
+        if (!should_unset((*env)[i], args)) {
+            newenv[j] = my_strdup((*env)[i]);
+            j++;
+        }
+    }
+    newenv[j] = NULL;
+    free(*env);
+    *env = newenv;
+    *status = 0;
 }
