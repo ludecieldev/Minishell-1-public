9b049411cac2fcbdca9f843b39a07bf8aad649e7
Alexandre Garbé <alexandre.garbe@epitech.eu>
2024-02-29 21:10:27 +0100

    fix: updated cd_no_args.c to get good status

--- src/shell/custom_args/cd/cd_no_args.c
+++ src/shell/custom_args/cd/cd_no_args.c
@@ -11,5 +11,18 @@
 
 void cd_no_args(char **args, char ***env, int *status)
 {
-    cd_tilde(args, env, status);
+    char *home;
+
+    (void)args;
+    (void)status;
+    update_wd(env);
+    home = my_strcat("/home/", my_getenv("USER", env));
+    mini_printf("home = %s\n", home);
+    if (chdir(home) == -1) {
+        puterror(home);
+        puterror(": No such file or directory.\n");
+        *status = 1;
+        return;
+    }
+    *status = 0;
 }
