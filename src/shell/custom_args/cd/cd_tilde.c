d34d53a88b76d155f472a3605d6e8f07110c20c4
Alexandre Garbé <alexandre.garbe@epitech.eu>
2024-02-29 21:10:36 +0100

    fix: updated cd_tilde.c to get good status

--- src/shell/custom_args/cd/cd_tilde.c
+++ src/shell/custom_args/cd/cd_tilde.c
@@ -13,10 +13,21 @@ void cd_tilde(char **args, char ***env, int *status)
 {
     char *home;
 
+    if (args[2] != NULL) {
+        puterror("cd: Too many arguments.\n");
+        *status = 1;
+        return;
+    }
     (void)args;
     (void)status;
     update_wd(env);
     home = my_strcat("/home/", my_getenv("USER", env));
-    printf("home = %s\n", home);
-    chdir(home);
+    mini_printf("home = %s\n", home);
+    if (chdir(home) == -1) {
+        puterror(home);
+        puterror(": No such file or directory.\n");
+        *status = 1;
+        return;
+    }
+    *status = 0;
 }
