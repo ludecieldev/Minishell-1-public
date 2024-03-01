e9e1b5f4c118fa7304718e4fb6814f7f63b56438
Alexandre Garbé <alexandre.garbe@epitech.eu>
2024-03-01 17:38:11 +0100

    feat: updated cd_checked.c

--- src/shell/custom_args/cd/cd_checked.c
+++ src/shell/custom_args/cd/cd_checked.c
@@ -11,12 +11,19 @@
 
 void check_cd(char **args, char ***env, int *status)
 {
+    char *oldpwd = my_getenv("OLDPWD=", env);
+
+    if (args[2] != NULL) {
+        puterror("cd: Too many arguments.\n");
+        *status = 1;
+        return;
+    }
     if (args[1] == NULL) {
         cd_no_args(args, env, status);
         return;
     }
     if (my_strcmp(args[1], "-") == 0) {
-        cd_dash(args, env, status);
+        cd_dash(args, env, status, oldpwd);
         return;
     }
     if (args[1][0] == '~') {
