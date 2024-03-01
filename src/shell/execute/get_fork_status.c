fb129872174951d6f9794f4a9a523679301448fb
Alexandre Garbé <alexandre.garbe@epitech.eu>
2024-03-01 18:53:49 +0100

    feat: edited fork_status

--- src/shell/execute/get_fork_status.c
+++ src/shell/execute/get_fork_status.c
@@ -14,11 +14,19 @@ int get_fork_status(int status)
     if (WIFEXITED(status))
         return WEXITSTATUS(status);
     if (WIFSIGNALED(status)) {
-        if (WTERMSIG(status) == 11)
+        if (WTERMSIG(status) == SIGSEGV) {
             mini_printf("Segmentation fault\n");
-        if (WTERMSIG(status) == 8)
+            return 139;
+        }
+        if (WTERMSIG(status) == SIGFPE) {
             mini_printf("Floating exception\n");
-        return 139;
+            return 136;
+        }
+        if (WTERMSIG(status) == SIGILL) {
+            mini_printf("Illegal instruction (possible wrong architecture error)\n");
+            return 132;
+        }
     }
     return 0;
 }
+
